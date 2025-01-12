d2b010c267292e0c45c4798d46e14e787f654838
DrDisagree <crazymahmud08@gmail.com>
2025-01-13 01:56:15 +0600

    Improve card view texts

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -48,43 +48,43 @@ const page = () => {
           Generate resume in just 3 steps
         </h2>
 
-        <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 md:px-24">
-          <div className="cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300">
+        <div className="mt-8 grid grid-cols-1 gap-8 text-center md:text-start md:grid-cols-2 lg:grid-cols-3 md:px-24">
+          <div className="flex flex-col cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300 items-center md:items-start justify-center md:justify-start">
             <AtomIcon className="h-8 w-8" />
 
             <h2 className="mt-4 text-xl font-bold text-black">
               Create Your Template
             </h2>
 
-            <p className="mt-1 text-sm text-gray-600">
+            <p className="mt-1 text-sm text-gray-600 md:text-justify">
               Start by selecting the color scheme for your resume template. Our
               single, professionally designed template ensures a clean and
               consistent look for all users.
             </p>
           </div>
 
-          <div className="cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300">
+          <div className="flex flex-col cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300 items-center md:items-start justify-center md:justify-start">
             <Edit className="h-8 w-8" />
 
             <h2 className="mt-4 text-xl font-bold text-black">
               Update Your Information
             </h2>
 
-            <p className="mt-1 text-sm text-gray-600">
+            <p className="mt-1 text-sm text-gray-600 md:text-justify">
               Enter your personal details, work experience, education, and
               skills into the provided form. Our AI assists you in filling out
               each section accurately and effectively.
             </p>
           </div>
 
-          <div className="cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300">
+          <div className="flex flex-col cursor-pointer p-8 border border-gray-100 rounded-3xl bg-white shadow-xl max-md:shadow-md shadow-gray-600/10 hover:shadow-gray-600/15 transition-shadow duration-300 items-center md:items-start justify-center md:justify-start">
             <Share2 className="h-8 w-8" />
 
             <h2 className="mt-4 text-xl font-bold text-black">
               Share Your Resume
             </h2>
 
-            <p className="mt-1 text-sm text-gray-600">
+            <p className="mt-1 text-sm text-gray-600 md:text-justify">
               After completing your resume, save it securely and generate a
               shareable link. Easily update your information anytime and share
               the link with potential employers or download it in a preferred
