06b45034f9279e303e67879866e771272d942b86
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-03-14 14:30:42 +0600

    Update gemini model version

--- lib/actions/gemini.actions.ts
+++ lib/actions/gemini.actions.ts
@@ -9,7 +9,7 @@ import {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
 });
 
 const generationConfig = {
